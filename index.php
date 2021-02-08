9af9cdf75d35b3f08f2496c93f23926bba74e209
ibooio <iboosocial@gmail.com>
2021-02-08 15:43:14 -0300

    second commit
    fix structure

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php echo "first commit"?> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Administrador de usuarios</title>
+        <link rel="stylesheet" href="assets/css/app.css" type="text/css">
+    </head>
+    <body>
+        <div>Second</div>
+        <script src="assets/js/app.js"></script>
+    </body>
+</html>
